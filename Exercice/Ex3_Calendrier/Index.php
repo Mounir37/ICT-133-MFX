7555eebe73fef7d93ee5c345081e68c731329169
Mounir37 <47849542+Mounir37@users.noreply.github.com>
2019-12-06 15:01:24 +0100

    6.12.19

--- Exercice/Ex3_Calendrier/Index.php
+++ Exercice/Ex3_Calendrier/Index.php
@@ -16,41 +16,28 @@
 $monthQS = $_GET['month'];
 $yearQS = $_GET['year'];
 $month = array("Janvier", "Février", "Mars", "Avril", "Mai", "juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Décembre");
-$year = array("2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023", "2024", "2025", );
-echo "<div class=\"month\">
-	  	<ul>
-	  		<li>August<br>2017</li>
-	  	</ul>
-	  </div>";
+$weeks = array("Lu", "Ma", "Me", "Je", "Ve", "Sa", "Di");
 ?>
 <div class="month">
     <ul>
-        <li>August<br>2017</li>
+		<?php
+		    echo "<li>" . $month[$monthQS - 1] . "<br>$yearQS</li>";
+		?>
     </ul>
 </div>
-
-<ul class="weekdays">
-    <li>Mo</li>
-    <li>Tu</li>
-    <li>We</li>
-    <li>Th</li>
-    <li>Fr</li>
-    <li>Sa</li>
-    <li>Su</li>
-</ul>
-
 <ul class="days">
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
-    <li>4</li>
-    <li>5</li>
-    <li>6</li>
-    <li>7</li>
-    <li>8</li>
-    <li>9</li>
-    <li><span class="active">10</span></li>
-    <li>11</li>
+	<?php
+        for ($i = 0; $i < 7; $i++){
+            echo "<li>$weeks[$i]</li>";
+        }
+	?>
+</ul>
+<ul>
+    <?php
+    for ($i = 1;;){
+
+    }
+    ?>
 </ul>
 </body>
 </html>
