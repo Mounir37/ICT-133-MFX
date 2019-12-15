a6edec076d32ac28b0a778c97991a39fd0c5058e
Mounir37 <47849542+Mounir37@users.noreply.github.com>
2019-12-15 17:01:19 +0100

    15.12.19

--- Exercice/Ex3_Calendrier/Index.php
+++ Exercice/Ex3_Calendrier/Index.php
@@ -1,43 +1,92 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>Exo Calendrier</title>
     <link rel="stylesheet" href="CSS/style.css">
 </head>
 <body>
 <?php
 /**
- *Auteur:   Mounir Fiaux
- *Titre:    Index.php
- *Date:     05.12.2019
- *Version:
+ *  Projet: Calendrier
+ *  Repos: ICT-133-SRD
+ *  Author: Mounir Fiaux
+ *  Creation date: 15.12.2019
  */
-$monthQS = $_GET['month'];
-$yearQS = $_GET['year'];
-$month = array("Janvier", "Février", "Mars", "Avril", "Mai", "juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Décembre");
-$weeks = array("Lu", "Ma", "Me", "Je", "Ve", "Sa", "Di");
-?>
-<div class="month">
-    <ul>
-		<?php
-		    echo "<li>" . $month[$monthQS - 1] . "<br>$yearQS</li>";
-		?>
-    </ul>
-</div>
-<ul class="weekdays">
-	<?php
-        for ($i = 0; $i < 7; $i++){
-            echo "<li>$weeks[$i]</li>";
-        }
-	?>
-</ul>
-<ul class="days">
-    <?php
-    for ($i = 1;$i < 31; $i++){
-        echo "<li>$i</li>";
+$month = 8;
+$year = 2019;
+if (isset($_GET['month']) && isset($_GET['year'])) {
+    $month = $_GET['month'];
+    $year = $_GET['year'];
+}
+$now = time();
+$year += 0;
+$month += 0;
+$mois_avant = $month - 1;
+$mois_apres = $month + 1;
+$annee_avant = $year - 1;
+$annee_apres = $year + 1;
+
+if ($month == 1) {
+    $mois_avant = 12;
+    $mois_apres = 2;
+} else if ($month == 12) {
+    $mois_apres = 1;
+    $mois_avant = 11;
+    $yeartomonth = $annee_apres;
+} else {
+    $mois_avant = $month - 1;
+    $mois_apres = $month + 1;
+    $yeartomonth = $year;
+}
+$jour_commencement = date("w", strtotime("$year-$month-0"));
+$jour_commencement += 0;
+echo $jour_commencement;
+$lastdaypossiblelastmonth = 31;
+while (checkdate($mois_avant, $lastdaypossiblelastmonth, $year) != true) {
+    $lastdaypossiblelastmonth--;
+}
+$lastdaypossiblethismonth = 31;
+while (checkdate($month, $lastdaypossiblethismonth, $year) != true) {
+    $lastdaypossiblethismonth--;
+}
+
+$month = date("F");
+
+echo '<div class="month"><ul>';
+echo "<li>$month<br>$year</li>";
+echo "</div></ul>";
+echo '<ul class="weekdays">';
+for ($i = 1; $i <= 7; $i++) {
+    //Generate days of the week:
+    $dayinrun = date("l");
+    if ($dayinrun == date("l", $now) && $month == date("F", $now) && $year == date("Y", $now)) {
+        echo "<li><span class='active'>$dayinrun</span></li>";
+    } else {
+        echo "<li>$dayinrun</li>";
     }
-    ?>
-</ul>
+}
+echo '</ul>';
+$nbdayslastmonth = $jour_commencement;
+echo '<ul class="days">';
+
+if ($jour_commencement != 0) {
+    while ($jour_commencement != 0) {
+        $daylastmonth = $lastdaypossiblelastmonth - $jour_commencement + 1;
+        $jour_commencement -= 1;
+        echo "<li class='lastmonth'>$daylastmonth</li>";
+    }
+}
+
+for ($i = 1; $i <= $lastdaypossiblethismonth; $i++) {
+    if ($i == date("j", $now) && $month == date("F", $now) && $year == date("Y", $now)) {
+        echo "<li class='thismonth'><span class='active'>$i</span></li>";
+    } else {
+        echo "<li class='thismonth'>$i</li>";
+    }
+}
+echo '</ul>';
+?>
+
 </body>
 </html>
